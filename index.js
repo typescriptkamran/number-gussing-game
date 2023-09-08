a91d4348ea075f53dd9f14e9d0b5bde9d19f4514
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-09-08 23:49:36 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const targetNumber = Math.floor(Math.random() * 100) + 1;
+export {};
+// console.log(targetNumber)
---
+++ index.ts
@@ -0,0 +1,4 @@
+
+
+const targetNumber: number = Math.floor(Math.random()*100)+1 
+// console.log(targetNumber)
